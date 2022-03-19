5c428bdc86f9d91c9141fe0a03217d60da97d736
Zolival <zolinak@gmail.com>
2022-03-19 18:59:07 +0100

    *

---
+++ szombat/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAl-f-D1KTAOp7c-6js1NyPz8RAC5KVLeo",
+  authDomain: "szombat2.firebaseapp.com",
+  projectId: "szombat2",
+  storageBucket: "szombat2.appspot.com",
+  messagingSenderId: "852902762080",
+  appId: "1:852902762080:web:b7335405754103d4121505"
+};
+
+export const environment = {
+  production: true,
+  firebaseConfig
+};
